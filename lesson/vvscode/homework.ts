5e802283104b160e5526d7cede154a80bd85f3e4
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-11-30 00:13:14 +0400

    summator

--- lesson/vvscode/homework.ts
+++ lesson/vvscode/homework.ts
@@ -7,3 +7,19 @@ function isInArray<T>(list: T[], ...params: T[]): boolean {
   // тут вопрос - зачем мне явно указывать тип `el`, если его можно вывести?
   return params.every((el: T) => list.includes(el));
 }
+
+/*
+ Написать функцию summator(), которая суммирует переданые ей аргументы.
+ Аргументы могут быть либо строкового либо числового типа. Количество их не ограничено
+*/
+function summator(...params: (string | number)[]): number {
+  return params.reduce((acc: number, el: string | number) => {
+    let num: number;
+    if (typeof el === 'string') {
+      num = parseInt(el, 10);
+    } else {
+      num = el;
+    }
+    return acc + num;
+  }, 0);
+}
