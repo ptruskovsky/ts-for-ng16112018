b720354ce2b8c137f3e5a98e0a87019bcf86dd62
ptruskovsky <ptruskovsky@gmail.com>
2018-12-04 10:04:39 +0300

    HomeWork #1 by Pavel Truskovsky

---
+++ lesson/TruskovskyPavel/out/homework.d.ts
@@ -0,0 +1,8 @@
+declare class Homework {
+    constructor();
+    isInArray(): void;
+    summator(): void;
+    getUnique(arr: []): void;
+    toMatrix(data: [], rowSize: number): void;
+}
+declare let test: Homework;
