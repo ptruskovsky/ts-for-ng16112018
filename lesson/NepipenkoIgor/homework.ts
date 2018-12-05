3101284f45d0ef47284a213843cb238e9eba5402
ashku <ashkurpela@deloitte.com>
2018-12-05 14:53:24 +0200

    WIP 3 function have been implemented but 2 & 3 with errors

--- lesson/NepipenkoIgor/homework.ts
+++ lesson/NepipenkoIgor/homework.ts
@@ -1,3 +1,4 @@
+//1
 function isInArray (param1: any[], ...param2: any[]): boolean {
     const argsLen: number = param2.length;
     for(let i: number = argsLen-1; i >= 0; i-- ){
@@ -7,6 +8,26 @@ function isInArray (param1: any[], ...param2: any[]): boolean {
     }
     return true;
 }
-console.log(isInArray([1, 2, 3, 5, 'qwe'], 1, 2, 3, 5, 'qwe'));
+// console.log(isInArray([1, 2, 3, 5, 'qwe'], 1, 2, 3, 5, 'qwe')); //true
 
-function summator () {}
+//2
+// function summator ( ...param: (number|string)[]): any {
+//     return param.reduce((accumulator: number, currentValue: number|string): number => {
+//         return parseFloat(accumulator) + parseFloat(currentValue);
+//     });
+// }
+//
+// console.log(summator(1, 2, '3')); //6
+
+//3
+// function getUnique(...arr: any[]): any[]{
+//     return [...new Set(arr)];
+// }
+//
+// console.log(getUnique(1, 2, 'we', 1, 'we', 'rrr')); //[1, 2, 'we', 'rrr']
+
+//4
+// function toMatrix (data: any[], rowSize: number): any[]{
+//     return [];
+// }
+
